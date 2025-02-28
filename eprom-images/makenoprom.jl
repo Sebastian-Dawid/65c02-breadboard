b38a6992f9127b047b2abfa0b445bddace1251fe
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2025-02-28 18:53:00 +0100

    Program now sets up 64c22 port B for writes and alternates writing 0x42 and 0xaa to the port in a loop

--- eprom-images/makenoprom.jl
+++ eprom-images/makenoprom.jl
@@ -2,17 +2,30 @@ function write_byte(addr, rom, byte)
     rom[addr + 1] = byte
 end
 
+code_addr = 0x0000
+
+function write_instruction(rom, bytes)
+    for i in eachindex(bytes)
+        write_byte(code_addr, rom, bytes[i])
+        global code_addr += 1
+    end
+end
+
 nop = ones(UInt8, 32768) .* 0xea
+
+write_instruction(nop, [0xa9, 0xff])
+write_instruction(nop, [0x8d, 0x02, 0x60])
+
+write_instruction(nop, [0xa9, 0x42])
+write_instruction(nop, [0x8d, 0x00, 0x60])
+
+write_instruction(nop, [0xa9, 0xaa])
+write_instruction(nop, [0x8d, 0x00, 0x60])
+
+write_instruction(nop, [0x4c, 0x05, 0x80])
 
 write_byte(0x7ffc, nop, 0x00)
 write_byte(0x7ffd, nop, 0x80)
-
-write_byte(0x0000, nop, 0xa9)
-write_byte(0x0001, nop, 0x42)
-
-write_byte(0x0002, nop, 0x8d)
-write_byte(0x0003, nop, 0x00)
-write_byte(0x0004, nop, 0x70)
 
 open("nop.bin", "w") do f
     write(f, nop)
